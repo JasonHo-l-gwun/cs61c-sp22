74f0568b8d237b5f3af3760d2e06d87be76c03ff
Jason Ho <3194739773@qq.com>
2025-05-24 14:29:45 +0800

    Done with lab06 and lab 07

--- lab07/cache.s
+++ lab07/cache.s
@@ -22,10 +22,10 @@
 array:	.word	2048 # max array size specified in BYTES (DO NOT CHANGE)
 
 .text
-main:	li	a0, 128	# array size in BYTES (should be a power of 2 < array size)
-	li	a1, 1 # step size  (should be a power of 2 > 0)
+main:	li	a0, 256	# array size in BYTES (should be a power of 2 < array size)
+	li	a1, 2 # step size  (should be a power of 2 > 0)
 	li	a2, 1 # rep count  (int > 0)
-	li	a3, 0 # 0 = option 0, 1 = option 1
+	li	a3, 1 # 0 = option 0, 1 = option 1
 	jal	accessWords	
 	li	a0, 10 # exit
 	ecall
@@ -42,12 +42,12 @@ accessWords:
 wordLoop:
 	beq	a3, zero,  optionZero
     # Option 1:
-	lw	t0, 0(s0)
+	lw	t0, 0(s0) # break
 	addi t0, t0, 1
-	sw t0, 0(s0)
+	sw t0, 0(s0) # break
 	j wordCheck
 optionZero:
-	sw zero, 0(s0)
+	sw zero, 0(s0) # break
 wordCheck:
 	add	s0, s0, t1 # increment ptr
 	blt	s0, s1, wordLoop # inner loop done?
